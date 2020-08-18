6220a255508a6439cd858d900cc6d9f8396eafd2
RubenOdd <sardorbekmukhudinov@gmail.com>
2020-08-18 09:52:09 +0300

    18.8 harjoitus

---
+++ jscode.js
@@ -0,0 +1,9 @@
+let nimi = prompt("Anna nimesi: ");
+
+if (nimi == "Sardor") {
+    console.log("Welcome Admin");
+}
+else if (nimi == "Mitja") {
+    console.log(nimi + " dumb!");
+}
+else console.log("Welcome " + nimi);
